585af187263393045ae7b40010674fa745e8850b
Glenn Reyes <glenn@glennreyes.com>
2024-01-16 14:46:15 +0100

    Update linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,4 @@
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
   extends: ['@napaling'],
-  rules: {
-    '@stylistic/lines-around-comment': [
-      'error',
-      { afterHashbangComment: true },
-    ],
-    '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true }],
-    '@typescript-eslint/sort-type-constituents': 'off',
-    'import/consistent-type-specifier-style': 'error',
-    'unicorn/no-array-reduce': 'off',
-  },
 };
